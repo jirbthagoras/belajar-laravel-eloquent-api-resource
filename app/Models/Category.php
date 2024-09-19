81bc3294b9cc85f18ab115adccc231c2ace68949
jirbthagoras <hansjabriel@gmail.com>
2024-09-19 08:51:51 +0700

    Making Category Model Resource

---
+++ app/Models/Category.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+
+class Category extends Model
+{
+    use HasFactory;
+
+    protected $table = "categories";
+    protected $primaryKey = "id";
+    protected $keyType = 'int';
+    public $incrementing = true;
+    public $timestamps = true;
+
+    public function product(): HasMany
+    {
+        return $this->hasMany(Product::class, "category_id", "id");
+    }
+}
---
+++ app/Models/Product.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Models;
+
+use Database\Seeders\CategorySeeder;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
+class Product extends Model
+{
+    use HasFactory;
+
+    protected $table = 'products';
+    protected $primaryKey = 'id';
+    protected $keyType = "int";
+    public $incrementing = true;
+    public $timestamps = true;
+
+    public function categories(): BelongsTo
+    {
+        return $this->belongsTo(Category::class, "category_id", "id");
+    }
+}
